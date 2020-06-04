7228b257882ee6e4e3a729461b266bd7919d6e35
Paul-Austria <1paulengelhardt@gmail.com>
2020-06-04 09:20:05 +0200

    working!

--- src/main/java/UsCurrencyChecker.java
+++ src/main/java/UsCurrencyChecker.java
@@ -1,9 +1,13 @@
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+//import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+//import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.lang.*;
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UsCurrencyChecker {
     public static Matcher getMatcher(String usCurrencyString) {
-        throw new NotImplementedException();
+        Pattern pattern = Pattern.compile("\\$\\s?\\d+(\\.\\d{1,2})?");
+        return  pattern.matcher(usCurrencyString);
     }
 }
